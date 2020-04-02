d1c5aadc65cbb9ec131ba2fbfff1d6156625ca08
jiaotou <jiaotou@2dfire.com>
2020-04-02 15:21:32 +0800

    单例模式枚举模式写法

---
+++ src/main/java/com/wushuang/pattern/singleton/Singleton5.java
@@ -0,0 +1,12 @@
+package com.wushuang.pattern.singleton;
+
+/**
+ * 单例模式 枚举模式写法
+ * 默认枚举实例的创建是线程安全的，并且在任何情况下都是单例
+ *
+ * @author <a href="jiaotou@2dfire.com">jiaotou</a>
+ * @date 2020/4/2 15:20
+ */
+public enum Singleton5 {
+    INSTANCE;
+}
